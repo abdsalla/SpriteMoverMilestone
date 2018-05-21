75c2b9f5f2725f28c7f0b17c4772f86aa2b1769c
Duli <ablais147@gmail.com>
2018-05-20 20:14:20 -1000

    Comments
    Added comments for the sprite mover milestone script

--- Sprite Mover Milestone/Assets/SpriteMover.cs
+++ Sprite Mover Milestone/Assets/SpriteMover.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpriteMover : MonoBehaviour {
-    private Transform Spritetransform;
-    public float movespeed;
+    private Transform Spritetransform; // creating a Transform called Spritetransform for later use
+    public float movespeed; // creating a public float so that our designer can change the speed without going into the code
 
     // Use this for initialization
     void Start() {
-        Spritetransform = GetComponent<Transform>();
+        Spritetransform = GetComponent<Transform>(); // storing our gameObjects transform information within it with GetComponent
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Spritetransform.position = Spritetransform.position + (Vector3.right * movespeed);      
+        Spritetransform.position = Spritetransform.position + (Vector3.right * movespeed);   // this will move our sprite in the direction it is facing multiplied by the movespeed float   
 	}
 }
