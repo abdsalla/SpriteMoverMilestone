ed0031f03a0c0a2103e6c792b44b79df47396b2f
Duli <ablais147@gmail.com>
2018-05-20 15:24:09 -1000

    Added files, and completed milestone1-1
    Completed Milestone 1-1

---
+++ Sprite Mover Milestone/Assets/SpriteMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMover : MonoBehaviour {
+    private Transform Spritetransform;
+    public float movespeed;
+
+    // Use this for initialization
+    void Start() {
+        Spritetransform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Spritetransform.position = Spritetransform.position + (Vector3.right * movespeed);      
+	}
+}
